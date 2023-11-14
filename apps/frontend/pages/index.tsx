4e972d7af7c168ba19000d9a788f33a8bb65b2c2
Antoine Labalette <labalette.antoine@gmail.com>
2023-11-14 17:22:37 +0100

    setup loggin, register and auth

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,5 +1,6 @@
 import { log } from "logger";
 import { CounterButton, Link } from "ui";
+import axios from "axios"
 
 export const metadata = {
   title: "Store | Kitchen Sink",
@@ -8,23 +9,60 @@ export const metadata = {
 export default function Store(): JSX.Element {
   log("Hey! This is the Store page.");
 
+  const sayHello = async () => {
+	try
+	{
+		const res = await axios.get("http://localhost:5001/")
+		console.log(res.data)
+	}
+	catch (error) {
+		console.log(error)
+	}
+  }
+
+  const login = async () => {
+	try {
+		const mail = "tonio@example.com"
+		const pwd = "Tonio"
+		const res = await axios.post("http://localhost:5001/api/login", {Email: mail, Password: pwd})
+		console.log(res.data)
+	} catch (error) {
+		console.log(error)
+	}
+  }
+
+  const register = async () => {
+	try {
+		const uname = "tonio"
+		const mail = "tonio@example.com"
+		const pwd = "Tonio"
+		const res = await axios.post("http://localhost:5001/api/register", {Username: uname, Email: mail, Password: pwd})
+		console.log(res.data)
+	} catch (error) {
+		console.log(error)
+	}
+  }
+
+  const test = async () => {
+	try {
+		const token = (document.getElementById("token") as HTMLInputElement).value
+		const res = await axios.post("http://localhost:5001/api/test", {token: token})
+		console.log(res.data)
+	} catch (error) {
+		console.log(error)
+	}
+  }
+
   return (
-    <div className="container">
-      <h1 className="title">
-        myyyy <br />
-        <span>Kitchen Sink</span>
-      </h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <Link href="https://turbo.build/repo" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://nextjs.org/" newTab>
-          Next.js
-        </Link>
-      </p>
-    </div>
+	<>
+    	<button onClick={sayHello}>Say Hello</button>
+		<br />
+		<button onClick={login}>Login user1</button>
+		<br />
+		<button onClick={register}>Register new user</button>
+		<br />
+		<input id="token" />
+		<button onClick={test}>Test token</button>
+	</>
   );
 }
