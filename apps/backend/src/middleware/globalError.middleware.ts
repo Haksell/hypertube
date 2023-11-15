1bdea2816352d65dbeab634a95fcbc7f11ef5317
Antoine Labalette <labalette.antoine@gmail.com>
2023-11-15 13:06:47 +0100

    refactor

--- apps/backend/src/middleware/globalError.middleware.ts
+++ apps/backend/src/middleware/globalError.middleware.ts
@@ -8,7 +8,7 @@ const globalErrorMiddleware = (
 	next: NextFunction
 ) => {
 	console.error(err.stack);
-	return res.status(500).send("Une erreur est survenue sur le serveur.");
+	return res.sendStatus(500);
 };
 
 export default globalErrorMiddleware;
