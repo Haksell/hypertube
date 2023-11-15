aebd8abf90bcea7b30313c05dc816b1c2eb8359e
Axel Brisse <axel.brisse@gmail.com>
2023-11-15 15:09:38 +0100

    film -> movie

--- apps/frontend/pages/movies.tsx
+++ apps/frontend/pages/movies.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 
-type Film = {
+type Movie = {
 	title: string
 	thumbnail: string
 	year: number
@@ -9,7 +9,7 @@ type Film = {
 	imdbRating?: number
 }
 
-let films: Film[] = [
+let movies: Movie[] = [
 	{
 		title: 'Oppenheimer',
 		thumbnail: '/dev_thumbnails/oppenheimer.jpg',
@@ -56,18 +56,18 @@ let films: Film[] = [
 	},
 ]
 
-films = [
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
-	...films,
+movies = [
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
+	...movies,
 ]
 
 const formatDuration = (minutes: number) => {
@@ -76,7 +76,7 @@ const formatDuration = (minutes: number) => {
 	return `${hours}:${mins}`
 }
 
-const FilmCard: React.FC<{ film: Film }> = ({ film }) => {
+const MovieCard: React.FC<{ movie: Movie }> = ({ movie }) => {
 	const [isHovered, setIsHovered] = useState(false)
 
 	return (
@@ -86,8 +86,8 @@ const FilmCard: React.FC<{ film: Film }> = ({ film }) => {
 			onMouseLeave={() => setIsHovered(false)}
 		>
 			<img
-				src={film.thumbnail}
-				alt={film.title}
+				src={movie.thumbnail}
+				alt={movie.title}
 				width={230}
 				height={345}
 				className={`w-full h-auto transition-all duration-300 ease-in-out ${
@@ -96,27 +96,29 @@ const FilmCard: React.FC<{ film: Film }> = ({ film }) => {
 			/>
 			{isHovered && (
 				<div className="absolute inset-0 bg-black bg-opacity-75 text-white p-4 flex flex-col justify-end transition-opacity duration-300 ease-in-out">
-					<h3 className="text-base md:text-lg lg:text-xl font-bold mb-2">{film.title}</h3>
-					{film.imdbRating && (
-						<div className="text-center text-sm">{film.imdbRating} / 10</div>
+					<h3 className="text-base md:text-lg lg:text-xl font-bold mb-2">
+						{movie.title}
+					</h3>
+					{movie.imdbRating && (
+						<div className="text-center text-sm">{movie.imdbRating} / 10</div>
 					)}
 					<div className="flex justify-between text-sm">
-						<span>{film.year}</span>
-						<span>{formatDuration(film.length)}</span>
+						<span>{movie.year}</span>
+						<span>{formatDuration(movie.length)}</span>
 					</div>
 				</div>
 			)}
 		</div>
 	)
 }
-const FilmsPage = () => {
+const MoviesPage = () => {
 	return (
 		<div className="min-h-screen bg-black">
 			<NavBar />
 			<div className="text-white">
 				<div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-1 p-4">
-					{films.map((film) => (
-						<FilmCard key={film.title} film={film} />
+					{movies.map((movie) => (
+						<MovieCard key={movie.title} movie={movie} />
 					))}
 				</div>
 			</div>
@@ -124,4 +126,4 @@ const FilmsPage = () => {
 	)
 }
 
-export default FilmsPage
+export default MoviesPage
