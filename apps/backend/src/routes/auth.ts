5e4b958cd72cc731dbc49ea9bf20e9cd8a80c16f
Antoine Labalette <labalette.antoine@gmail.com>
2023-11-15 12:27:16 +0100

    Refactoring

---
+++ apps/backend/src/routes/auth.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import asyncHandler from "express-async-handler";
+import { register, login } from "../controllers/auth";
+
+const router = express.Router();
+
+router.post("/register", asyncHandler(register));
+router.post("/login", asyncHandler(login));
+
+export default router;
