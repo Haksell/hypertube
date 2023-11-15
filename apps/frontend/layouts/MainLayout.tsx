fed96aa1ae50a5daf825552baebbb2722ec17628
Axel Brisse <axel.brisse@gmail.com>
2023-11-15 14:08:05 +0100

    remove error from turbo.json + simplify layout

--- apps/frontend/layouts/MainLayout.tsx
+++ apps/frontend/layouts/MainLayout.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
-import '../styles/tailwind.css';
+import React from 'react'
+import NavBar from '../components/NavBar'
+import '../styles/tailwind.css'
 
 const MainLayout = ({ children }) => {
-  return (
-    <div>
-      <NavBar />
-	  <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
-      {children}
-	  </div>
-      {/* Ajoutez d'autres éléments de mise en page ici */}
-    </div>
-  );
-};
+	return (
+		<div>
+			<NavBar />
+			<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+				{children}
+			</div>
+		</div>
+	)
+}
 
-export default MainLayout;
+export default MainLayout
