f3625975ef912af02b03055cedde438ea6c89ca7
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-11-16 13:40:55 +0100

    users/me return

--- apps/backend/src/controllers/users.ts
+++ apps/backend/src/controllers/users.ts
@@ -2,11 +2,25 @@ import { Request, Response } from 'express'
 import { NotConnected, SuccessMsg } from '../shared/msg-error'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import { TUserCookie } from '../types_backend/user-cookie';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 export async function getMe(req: Request, res: Response) {
 	try {
+		console.log('test')
 		const decoded: TUserCookie = await getUserFromRequest(req);
-		res.status(200).send({ msg: SuccessMsg, deco: decoded })
+		console.log('test')
+		const user = await prisma.user.findUnique({
+			where: {
+				username: decoded.username,
+			}
+		})
+		console.log('test')
+		if (!user)
+			res.status(401).send(NotConnected)
+
+		res.status(200).send({ msg: SuccessMsg, deco: user })
 	}
 	catch (error) {
 		res.status(401).send(NotConnected)
@@ -17,7 +31,9 @@ export async function getUserFromRequest(req: Request): Promise<TUserCookie> {
 	try {
 		const token = req.cookies.token
 		if (token) {
+			console.log('test1')
 			const decoded = await jwt.verify(token, process.env.TOKEN_KEY) as JwtPayload;
+			console.log('test2')
 			if (!decoded)
 				throw new Error(NotConnected)
 			const decodedCookie: TUserCookie = {
