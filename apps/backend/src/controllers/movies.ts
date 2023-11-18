a860adecb8f5f2682f118e04220965a524cf0f94
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-11-18 12:33:55 +0100

    deleting hardcoded movies

--- apps/backend/src/controllers/movies.ts
+++ apps/backend/src/controllers/movies.ts
@@ -11,53 +11,6 @@ type Movie = {
 	source: 'EZTV' | 'YTS'
 }
 
-let moviesHardCoded: Movie[] = [
-    {
-        title: 'Oppenheimer',
-        thumbnail: '/dev_thumbnails/oppenheimer.jpg',
-        year: 2023,
-        length: 162,
-        imdbRating: 8.5,
-    },
-    {
-        title: 'Rumble Through the Dark',
-        thumbnail: '/dev_thumbnails/rumble_through_the_dark.jpg',
-        year: 2023,
-        length: 162,
-        imdbRating: 6.2,
-    },
-    {
-        title: 'The Killer',
-        thumbnail: '/dev_thumbnails/the_killer.jpg',
-        year: 2023,
-        length: 162,
-    },
-    {
-        title: 'The Creator',
-        thumbnail: '/dev_thumbnails/the_creator.jpg',
-        year: 2023,
-        length: 162,
-    },
-    {
-        title: 'The Death',
-        thumbnail: '/dev_thumbnails/the_death.jpg',
-        year: 2023,
-        length: 162,
-    },
-    {
-        title: 'Spirit of Fear',
-        thumbnail: '/dev_thumbnails/spirit_of_fear.jpg',
-        year: 2023,
-        length: 162,
-    },
-    {
-        title: "OSS 117: Le Caire, nid d'espions",
-        thumbnail: '/dev_thumbnails/oss117.jpg',
-        year: 2006,
-        length: 99,
-    },
-]
-
 export async function getMovies(req: Request, res: Response) {
     const moviesYTS: Movie[] = await getMoviesFromYTS(15, 1)
 	const moviesEZTV: Movie[] = await getMoviesEZTV(5,1)
