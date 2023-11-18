07872040abf06026e3db0e57cb9c3aec693564cd
Nicolas LESAGE <nlesage@made-f0Dr11s12.clusters.42paris.fr>
2023-11-18 12:32:30 +0100

    fetching movies from YTS+EZTV

--- apps/backend/src/controllers/movies.ts
+++ apps/backend/src/controllers/movies.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { Request, Response } from 'express'
+import { getMoviesEZTV, getMoviesFromYTS } from '../utils/get-movies'
 
 type Movie = {
     title: string
@@ -7,6 +8,7 @@ type Movie = {
     year: number
     length: number
     imdbRating?: number
+	source: 'EZTV' | 'YTS'
 }
 
 let moviesHardCoded: Movie[] = [
@@ -57,100 +59,18 @@ let moviesHardCoded: Movie[] = [
 ]
 
 export async function getMovies(req: Request, res: Response) {
-    // const moviesYTS: Movie[] = await getMoviesFromYTS(20, 1)
-	const moviesYTS: Movie[] = await getMoviesEZTV(2,1)
-    res.status(201).send(moviesYTS)
+    const moviesYTS: Movie[] = await getMoviesFromYTS(15, 1)
+	const moviesEZTV: Movie[] = await getMoviesEZTV(5,1)
+
+	
+	// assemble both
+	let movies: Movie[] = []
+	if (moviesYTS && moviesYTS.length !== 0)
+		movies = movies.concat(moviesYTS)
+		
+	if (moviesEZTV && moviesEZTV.length !== 0)
+		movies = movies.concat(moviesEZTV)
+    res.status(201).send(movies)
 }
 
 // http://localhost:5001/movies?genra=love,comic&grade=5&prod=1998,1999&sort=downloads&limit=10&offset=10
-export async function getMoviesFromYTS(limit: number, page: number): Promise<Movie[]> {
-    try {
-        const response = await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=${limit}`)
-        const moviesYTS = response.data.data.movies
-        const movies: Movie[] = []
-
-        for (const elem of moviesYTS) {
-            const oneMovie: Movie = {
-                title: elem.title,
-                thumbnail: elem.large_cover_image,
-                year: elem.year,
-                length: elem.runtime,
-                imdbRating: elem.rating,
-            }
-			console.log(oneMovie.title + ' => '+ elem.imdb_code)
-            movies.push(oneMovie)
-        }
-        // console.log(movies)
-		return movies
-    } catch (error) {
-		return []
-    }
-}
-
-type InfoMovie = {
-	thumbnail: string;
-    year: number;
-    length: number;
-    imdbRating: number;
-}
-
-export async function getMoviesEZTV(limit: number, page: number): Promise<Movie[]> {
-	try {
-		const response = await axios.get(`https://eztv.re/api/get-torrents?limit=${limit}&page=${page}`)
-		// const response = await axios.get(`https://yts.torrentbay.net/api/v2/list_movies.json`)
-		const moviesEZTV = response.data.torrents
-		const movies: Movie[] = []
-
-		for (const elem of moviesEZTV) {
-			const info = await getInfoMovie(elem.imdb_id)
-			const oneMovie: Movie = {
-				title: elem.title,
-                thumbnail: 'http:' + elem.large_screenshot,
-                year: elem.year,
-                length: elem.runtime,
-                imdbRating: elem.rating,
-			}
-			console.log(oneMovie.title + ' => '+ elem.imdb_id)
-			movies.push(oneMovie)
-		}
-
-		// console.log(movies)
-		return movies
-
-		// https://yts.torrentbay.net/api/v2/list_movies.json
-        // const moviesEZ = response.data.data.movies
-		
-	}
-	catch (error) {
-		// console.log(error.response)
-		return []
-	}
-}
-
-async function getInfoMovie(movie_id: string): Promise<InfoMovie | null> {
-
-	try {
-		const response = await axios.get(`https://api.themoviedb.org/3/movie/tt${movie_id}`, {
-			headers: {
-			  Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDg3ODYyZmFlNzIzOTg1NGU0M2EwZWViOTY0ZTQ4OSIsInN1YiI6IjY1NTI0MzllMDgxNmM3MDBjM2Q5ZWI3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HLvzT_4FESs2smv0k8-zAqEXHgnLS4t9QFmnIwX4Nno`,
-			  accept: 'application/json',
-			}
-		  });
-		
-		const data = response.data
-		console.log(data)
-
-		// const info: InfoMovie = {
-		// 	thumbnail: string;
-		// 	year: number;
-		// 	length: number;
-		// 	imdbRating
-		// }
-		return null
-	}
-	catch (error ) {
-		console.log('cannot get '+movie_id)
-		console.log(error)
-		return null
-	}
-}
---
+++ apps/backend/src/utils/get-movies.ts
@@ -0,0 +1,101 @@
+export async function getMoviesFromYTS(limit: number, page: number): Promise<Movie[]> {
+    try {
+        const response = await axios.get(`https://yts.mx/api/v2/list_movies.json?limit=${limit}`)
+        const moviesYTS = response.data.data.movies
+        const movies: Movie[] = []
+
+        for (const elem of moviesYTS) {
+            const oneMovie: Movie = {
+                title: elem.title,
+                thumbnail: elem.large_cover_image,
+                year: elem.year,
+                length: elem.runtime,
+                imdbRating: elem.rating,
+				source: 'YTS'
+            }
+			// console.log(oneMovie.title + ' => '+ elem.imdb_code)
+            movies.push(oneMovie)
+        }
+        // console.log(movies)
+		return movies
+    } catch (error) {
+		return []
+    }
+}
+
+type InfoMovie = {
+	thumbnail: string;
+    year: number;
+    length: number;
+    imdbRating: number;
+}
+
+// get movies from EZTV source and update them with movieDB source
+export async function getMoviesEZTV(limit: number, page: number): Promise<Movie[]> {
+	try {
+		const response = await axios.get(`https://eztv.re/api/get-torrents?limit=${limit}&page=${page}`)
+		const moviesEZTV = response.data.torrents
+		const movies: Movie[] = []
+
+		for (const elem of moviesEZTV) {
+			let info = null
+			if (elem.imdb_id && elem.imdb_id !== 0) {
+				info = await getInfoMovie(`tt${elem.imdb_id}`)
+			}
+			let oneMovie: Movie = {
+				title: elem.title,
+                thumbnail: 'http:' + elem.large_screenshot,
+                year: elem.year,
+                length: elem.runtime,
+                imdbRating: elem.rating,
+				source: 'EZTV'
+			}
+			if (info) {
+				if (info.thumbnail !== '')
+					oneMovie.thumbnail = info.thumbnail
+				oneMovie.year = info.year
+				oneMovie.imdbRating = info.imdbRating
+
+				movies.push(oneMovie)
+			}			
+		}
+		return movies		
+	}
+	catch (error) {
+		return []
+	}
+}
+
+// use themoviedb source to extract informations about movies/tv_shows ()
+async function getInfoMovie(movie_id: string): Promise<InfoMovie | null> {
+	try {
+		const response = await axios.get(`https://api.themoviedb.org/3/find/${movie_id}?external_source=imdb_id`, {
+			headers: {
+				accept: 'application/json',
+				Authorization: `Bearer ${process.env.MOVIEDB_TOKEN}`,
+			}
+		  });
+		
+		const data = response.data
+		let image: string = ''
+
+		// if TV_result
+		if (data.tv_results.length !== 0) {
+			const tv = data.tv_results[0]
+			if (tv.poster_path)
+				image = `https://image.tmdb.org/t/p/w500${tv.poster_path}`
+			const year: number = parseInt(tv.first_air_date.substr(0,4))
+			return {
+				thumbnail: image,
+				year: year,
+				length: 0,
+				imdbRating: tv.vote_average,
+			}
+		}
+		return null
+	}
+	catch (error: any) {
+		// console.log('cannot get '+movie_id)
+		return null
+	}
+}
