5c1f4d7d667a141d9ff159704d119ff6f3f1768b
Nicolas <nicolas.lesage@hotmail.fr>
2023-11-18 13:30:03 +0100

    working on query params for movies route

--- apps/backend/src/controllers/movies.ts
+++ apps/backend/src/controllers/movies.ts
@@ -1,19 +1,56 @@
-import { Request, Response } from 'express'
-import { getMoviesEZTV, getMoviesFromYTS } from '../utils/get-movies'
 import { Movie } from '../types_backend/movies'
+import { getMoviesEZTV, getMoviesFromYTS } from '../utils/get-movies'
+import { Request, Response } from 'express'
 
-export async function getMovies(req: Request, res: Response) {
-    const moviesYTS: Movie[] = await getMoviesFromYTS(15, 1)
-	const moviesEZTV: Movie[] = await getMoviesEZTV(5,1)
-	
-	// assemble both
-	let movies: Movie[] = []
-	if (moviesYTS && moviesYTS.length !== 0)
-		movies = movies.concat(moviesYTS)
-		
-	if (moviesEZTV && moviesEZTV.length !== 0)
-		movies = movies.concat(moviesEZTV)
-    res.status(201).send(movies)
+export type TRequestGetMovie = {
+    genra: string[]
+    grade: number
+    prod: number
+    sort: string
+    limit: number
+    offset: number
 }
 
+class CustomError extends Error {
+    constructor(message: string) {
+        super(message)
+    }
+}
+
+export async function getMovies(req: Request, res: Response) {
+    try {
+        const params = convertRequestParams(req)
+        const moviesYTS: Movie[] = await getMoviesFromYTS(15, 1)
+        const moviesEZTV: Movie[] = await getMoviesEZTV(5, 1)
+
+        // assemble both
+        let movies: Movie[] = []
+        if (moviesYTS && moviesYTS.length !== 0) movies = movies.concat(moviesYTS)
+
+        if (moviesEZTV && moviesEZTV.length !== 0) movies = movies.concat(moviesEZTV)
+        res.status(201).send(movies)
+    } catch (error) {
+        if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
+        else res.status(400).send('Error')
+    }
+}
+
+export function convertRequestParams(req: Request): TRequestGetMovie {
+    const { genra, grade, prod, sort, limit, offset } = req.query
+	console.log('params: limit='+limit + ', offset='+offset)
+    if (!limit) throw new CustomError('params limit is mandatory')
+    if (!offset) throw new CustomError('params offset is mandatory')
+	if (!(limit instanceof String)) throw new CustomError('params limit must be unique')
+	// if (limit.lenght )
+	// const limitNB = parseInt(limit)
+    let params: TRequestGetMovie = {
+        genra: [],
+        grade: 0,
+        prod: 0,
+        sort: 'seed',
+        limit: 0,
+        offset: 0,
+    }
+    return params
+}
 // http://localhost:5001/movies?genra=love,comic&grade=5&prod=1998,1999&sort=downloads&limit=10&offset=10
--- apps/backend/src/types_backend/movies.ts
+++ apps/backend/src/types_backend/movies.ts
@@ -6,3 +6,23 @@ export type Movie = {
     imdbRating?: number
 	source: 'EZTV' | 'YTS'
 }
+
+export const genres = ["Action",
+"Adventure",
+"Animation",
+"Comedy",
+"Crime",
+"Documentary",
+"Drama",
+"Family",
+"Fantasy",
+"History",
+"Horror",
+"Music",
+"Mystery",
+"Romance",
+"Science-Fiction",
+"TV Movie",
+"Thriller",
+"War",
+"Western"]
