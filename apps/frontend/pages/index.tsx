21324e17e431aa6feefefa0211bc1d1f6acc708b
Nicolas <nicolas.lesage@hotmail.fr>
2023-11-21 15:26:46 +0100

    amending frontend for year io yearRange

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -252,8 +252,11 @@ const MoviesPage = () => {
                         label={t('index.year')}
                         handleChange={setYearRange}
                         options={[
-                            { value: '2022,2023', label: '2022 - now' },
-                            { value: '2021,2022', label: '2021 - 2022' },
+							{ value: '2024', label: '2024' },
+                            { value: '2023', label: '2023' },
+							{ value: '2022', label: '2022' },
+							{ value: '2021', label: '2021' },
+							{ value: '2020', label: '2020' },
                         ]}
                     />
                     <Filter
