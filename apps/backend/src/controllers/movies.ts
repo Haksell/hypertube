593ce2e0fd8610992df38e6e5975d2b4f9e6a6a4
Nicolas <nicolas.lesage@hotmail.fr>
2023-11-29 16:47:02 +0100

    subtitles conversion

--- apps/backend/src/controllers/movies.ts
+++ apps/backend/src/controllers/movies.ts
@@ -1,189 +1,189 @@
 import { CustomError, Movie, MovieDetails } from '../types_backend/movies'
-import { convertRequestParams, extractAllMoviesDownloaded, getMoviesEZTV, getMoviesFromYTS } from '../utils/get-movies'
-import { Request, Response } from 'express'
-import { addDetailsFromMovieDB, getInfoMovieTorrent, getMovieId } from '../utils/info-movie'
 import { createMovieDB, movieViewed } from '../utils/bdd-movie'
+import {
+    convertRequestParams,
+    extractAllMoviesDownloaded,
+    getMoviesEZTV,
+    getMoviesFromYTS,
+} from '../utils/get-movies'
+import { addDetailsFromMovieDB, getInfoMovieTorrent, getMovieId } from '../utils/info-movie'
+import {
+    addUserDetailsToMovie,
+    addUserDetailsToMoviesList,
+    getUserWithFavoritesAndViewed,
+} from '../utils/user-movie'
 import { PrismaClient, User } from '@prisma/client'
-import { addUserDetailsToMovie, addUserDetailsToMoviesList, getUserWithFavoritesAndViewed } from '../utils/user-movie'
+import { Request, Response } from 'express'
 
 const prisma = new PrismaClient()
 
 export async function getMovies(req: Request, res: Response) {
     try {
         const params = convertRequestParams(req)
-		console.log(params)
+        console.log(params)
 
-		const user: User = await getUserWithFavoritesAndViewed(req)
+        const user: User = await getUserWithFavoritesAndViewed(req)
 
-		let movies: Movie[] = []
+        let movies: Movie[] = []
 
-		// const repartition: number = Math.floor((params.limit / 4) * 3)
-		if (params.downloaded === 'no') {
-			if (params.type === 'movie') {
-				const moviesYTS: Movie[] = await getMoviesFromYTS(params.limit, params)
-				if (moviesYTS && moviesYTS.length !== 0) movies = movies.concat(moviesYTS)
-			}
-			else if (params.type === 'series') {
-				const moviesEZTV: Movie[] = await getMoviesEZTV(params.limit, params)
-				if (moviesEZTV && moviesEZTV.length !== 0) movies = movies.concat(moviesEZTV)
-			}
-			
-		}
-		else {
-			const moviesYTS: Movie[] = await extractAllMoviesDownloaded()
-			if (moviesYTS && moviesYTS.length !== 0) movies = movies.concat(moviesYTS)
-		}
+        // const repartition: number = Math.floor((params.limit / 4) * 3)
+        if (params.downloaded === 'no') {
+            if (params.type === 'movie') {
+                const moviesYTS: Movie[] = await getMoviesFromYTS(params.limit, params)
+                if (moviesYTS && moviesYTS.length !== 0) movies = movies.concat(moviesYTS)
+            } else if (params.type === 'series') {
+                const moviesEZTV: Movie[] = await getMoviesEZTV(params.limit, params)
+                if (moviesEZTV && moviesEZTV.length !== 0) movies = movies.concat(moviesEZTV)
+            }
+        } else {
+            const moviesYTS: Movie[] = await extractAllMoviesDownloaded()
+            if (moviesYTS && moviesYTS.length !== 0) movies = movies.concat(moviesYTS)
+        }
 
-		await addUserDetailsToMoviesList(user, movies)
-        
+        await addUserDetailsToMoviesList(user, movies)
+
         res.status(201).send(movies)
     } catch (error) {
         if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
         else res.status(400).send('Error')
-		console.log(error)
+        console.log(error)
     }
 }
 
 export async function getMovieInfo(req: Request, res: Response) {
-	try {
-		const movieId = getMovieId(req)
+    try {
+        const movieId = getMovieId(req)
 
-		const user: User = await getUserWithFavoritesAndViewed(req)
+        const user: User = await getUserWithFavoritesAndViewed(req)
 
-		//get info from YTS
-		let movie: MovieDetails = await getInfoMovieTorrent(movieId)
+        //get info from YTS
+        let movie: MovieDetails = await getInfoMovieTorrent(movieId)
 
-		//get info from TheMovieDB
-		await addDetailsFromMovieDB(movie)
+        //get info from TheMovieDB
+        await addDetailsFromMovieDB(movie)
 
-		//add info from user (already viewed / already liked)
-		await addUserDetailsToMovie(user, movie)
+        //add info from user (already viewed / already liked)
+        await addUserDetailsToMovie(user, movie)
 
-		// verif si film existe deja dans BDD. Si non, ajout dans BDD
-		await createMovieDB(movie)
+        // verif si film existe deja dans BDD. Si non, ajout dans BDD
+        await createMovieDB(movie)
 
-		//add movie to viewed By user
-		movieViewed(user, movieId)
-		
-		// console.log(movie)
-		res.status(201).send(movie)
-	}
-	catch (error) {
-		if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
+        //add movie to viewed By user
+        movieViewed(user, movieId)
+
+        // console.log(movie)
+        res.status(201).send(movie)
+    } catch (error) {
+        if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
         else res.status(400).send('Error')
-	}
-
+    }
 }
 
 export async function likeMovie(req: Request, res: Response) {
-	try {
-		//recupere user
-		const user = await getUserWithFavoritesAndViewed(req)
+    try {
+        //recupere user
+        const user = await getUserWithFavoritesAndViewed(req)
 
-		//verifie film existe
-		const movieId = getMovieId(req)
-		const movie = await prisma.movies.findMany({
-			where: {
-				imdb_code: movieId
-			}
-		})
-		if (!movie || movie.length !== 1) throw new CustomError('Wrong imdb code')
+        //verifie film existe
+        const movieId = getMovieId(req)
+        const movie = await prisma.movies.findMany({
+            where: {
+                imdb_code: movieId,
+            },
+        })
+        if (!movie || movie.length !== 1) throw new CustomError('Wrong imdb code')
 
-		//verif film deja liked
-		const alreadyLike: number = user.favoriteMovies.findIndex(elem => elem.movieId === movie[0].id)
+        //verif film deja liked
+        const alreadyLike: number = user.favoriteMovies.findIndex(
+            (elem) => elem.movieId === movie[0].id,
+        )
 
-		if (alreadyLike === -1) {
-			await prisma.favoriteMovie.create({
-				data: {
-					user: { 
-						connect: { id: user.id }
-					},
-					movie: {
-						connect: { id: movie[0].id }
-					},
-					imdb_code: movie[0].imdb_code
-				}
-			})
-			res.status(200).send('Movie liked')
-		}
-		else {
-			await prisma.favoriteMovie.delete({
-				where: {
-					id: user.favoriteMovies[alreadyLike].id
-				}
-			})
-			res.status(200).send('Movie unliked')
-		}
-	}
-	catch (error) {
-		if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
+        if (alreadyLike === -1) {
+            await prisma.favoriteMovie.create({
+                data: {
+                    user: {
+                        connect: { id: user.id },
+                    },
+                    movie: {
+                        connect: { id: movie[0].id },
+                    },
+                    imdb_code: movie[0].imdb_code,
+                },
+            })
+            res.status(200).send('Movie liked')
+        } else {
+            await prisma.favoriteMovie.delete({
+                where: {
+                    id: user.favoriteMovies[alreadyLike].id,
+                },
+            })
+            res.status(200).send('Movie unliked')
+        }
+    } catch (error) {
+        if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
         else res.status(400).send('Movie cannot be liked')
-	console.log(error)
-	}
+        console.log(error)
+    }
 }
 
 //get info from OpenSub
 
 export async function viewMovie(req: Request, res: Response) {
+    // const ffmpeg = require('fluent-ffmpeg');
 
-	// const ffmpeg = require('fluent-ffmpeg');
+    const path = require('path')
+    const fs = require('fs')
 
-	const path = require('path');
-	const fs = require('fs');
+    const videoPath = path.join('movies', `video.mp4`)
+    const stat = fs.statSync(videoPath)
 
-	const videoPath = path.join('movies', `video.mp4`);
-	const stat = fs.statSync(videoPath);
-  
-	res.writeHead(200, {
-	  'Content-Type': 'video/mp4',
-	  'Content-Length': stat.size,
-	});
-  
-	const videoStream = fs.createReadStream(videoPath);
-	videoStream.pipe(res);
+    res.writeHead(200, {
+        'Content-Type': 'video/mp4',
+        'Content-Length': stat.size,
+    })
 
-  
+    const videoStream = fs.createReadStream(videoPath)
+    videoStream.pipe(res)
 
-	// ffmpeg()
+    // ffmpeg()
     // .input(videoName)
-	// .videoCodec('libx264')
+    // .videoCodec('libx264')
     // .audioCodec('libmp3lame')
-	// .format('mpegts')
+    // .format('mpegts')
     // .on('end', function () {
-	// 	console.log('streaming video done')
-	// })
-	// .pipe(res, { end: true });
+    // 	console.log('streaming video done')
+    // })
+    // .pipe(res, { end: true });
 
-	// res.status(200).send('Movie')
+    // res.status(200).send('Movie')
 
-	
-	// const videoPath = path.join(__dirname, 'movies', videoName);
-  
-	// Envoyer la vidéo au client
-	// res.sendFile(fullfilepath);
-	// res.status(200).send('Movie viewed')
+    // const videoPath = path.join(__dirname, 'movies', videoName);
+
+    // Envoyer la vidéo au client
+    // res.sendFile(fullfilepath);
+    // res.status(200).send('Movie viewed')
 }
 
 export async function getSubtitle(req: Request, res: Response) {
-	const path = require('path');
+    const path = require('path')
 
-	const nameVttFile = 'newSub.vtt'
+    const nameVttFile = 'newSub.vtt'
 
-	const subPath = path.join('movies', `sub.srt`);
-	const vttPath = path.join('movies', nameVttFile);
+    const subPath = path.join('movies', `sub.srt`)
+    const vttPath = path.join('movies', nameVttFile)
 
-	var srt2vtt = require('srt-to-vtt')
-	var fs = require('fs')
-	
-	fs.createReadStream(subPath)
-	.pipe(srt2vtt())
-	.pipe(fs.createWriteStream(vttPath))
+    var srt2vtt = require('srt-to-vtt')
+    const fs = require('fs')
 
 
-	res.status(200).sendFile(process.cwd() + `/movies/${nameVttFile}`, (err) => {
-		if (err) {
-		  console.error('Erreur lors de l\'envoi du fichier :', err);
-		  res.status(500).send('Erreur interne du serveur');
-		}
-	  });
+    fs.createReadStream(subPath)
+    .pipe(srt2vtt())
+    .pipe(fs.createWriteStream(vttPath))
+
+    res.status(200).sendFile(process.cwd() + `/movies/${nameVttFile}`, (err) => {
+    	if (err) {
+    	  console.error('Erreur lors de l\'envoi du fichier :', err);
+    	  res.status(500).send('Erreur interne du serveur');
+    	}
+      });
 }
