236796bce57dd8587fe3d264f12be7e3cb0559a4
Axel Brisse <axel.brisse@gmail.com>
2023-12-05 15:47:00 +0100

    2:3 -> 2:03 for duration

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -152,10 +152,7 @@ const MoviesPage = () => {
                         />
                         <div
                             className="flex absolute items-center z-20 transition-all flex-row peer-checked:flex-row-reverse"
-                            style={{
-                                width: width2 + 'px',
-                                height: height2 + 'px',
-                            }}
+                            style={{ width: width2, height: height2 }}
                         >
                             <div className="font-bold py-1 px-3 mx-[8px] rounded-full bg-gradient-to-r from-orange-50 to-blue-50 transition-all text-transparent">
                                 {type === 'movie' ? t('index.movies') : t('index.tv')}
--- apps/frontend/src/utilsTime.ts
+++ apps/frontend/src/utilsTime.ts
@@ -1,5 +1,5 @@
 export const formatDuration = (minutes: number) => {
     const hours = Math.floor(minutes / 60)
     const mins = minutes % 60
-    return `${hours}:${mins}`
+    return `${hours}:${mins.toString().padStart(2, '0')}`
 }
