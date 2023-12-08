1407774441724d4b237eebebb34e35e81f3ffa63
Nicolas <nicolas.lesage@hotmail.fr>
2023-12-08 17:55:59 +0100

    implement getOneUser

--- apps/backend/src/api/users-api.ts
+++ apps/backend/src/api/users-api.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-import { CustomError } from '../types_backend/movies';
+import { CustomError, NotFoundError } from '../types_backend/movies';
 
 const prisma = new PrismaClient()
 
@@ -27,21 +27,26 @@ export async function getUsers(req: Request, res: Response) {
 
 export async function getOneUser(req: Request, res: Response) {
 	try {
-		let users = await prisma.user.findMany({
-			take: 50,
+		const { id } = req.params
+		if (!id) throw new CustomError('empty argument')
+		const numID = parseInt(id)
+		if (isNaN(numID)) throw new CustomError('incorrect ID format')
+		let user = await prisma.user.findUnique({
+			where: {
+				id: numID
+			},
 			select: {
-				id: true,
 				username: true,
+				email: true,
+				profile_picture: true,
 			}
 		})
-		if (!users) {
-			res.status(200).json({});
-			return
-		}
-		res.status(200).json(users);
+		if (!user) throw new NotFoundError('no user found with this ID')
+		res.status(200).json(user);
 	}
 	catch (error) {
 		if (error instanceof CustomError) res.status(400).send(`Invalid request: ${error.message}`)
+		else if (error instanceof NotFoundError) res.status(404).send(`Error: ${error.message}`)
         else res.status(400).send('Error with request')
 	}
 }
