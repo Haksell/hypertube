4a6e05ce757cb2a6c0a74c757aba1fc351f64d5b
Axel Brisse <axel.brisse@gmail.com>
2023-12-09 11:32:57 +0100

    center <Loading>

--- apps/frontend/components/Loading.tsx
+++ apps/frontend/components/Loading.tsx
@@ -2,7 +2,7 @@ import MainLayout from '../layouts/MainLayout'
 import React from 'react'
 
 export const LoadingNoLayout = () => (
-    <div>
+    <div className="w-full flex items-center justify-center">
         <img
             src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/725eef121244331.60c1c7928b5dd.gif"
             alt="loading"
